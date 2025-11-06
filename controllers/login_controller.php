19abd2f243c9cf7d2e56c7b498e012688ce3bb71
EdSoftware-cpu <ferchobp24@gmail.com>
2025-11-06 16:38:49 -0500

    Se agregó la opcion de Inactivar usarios

--- controllers/login_controller.php
+++ controllers/login_controller.php
@@ -10,7 +10,7 @@ $mysql->conectar();
 
 if (isset($_POST['email']) && isset($_POST['password'])) {
 
-    $email = $_POST['email'] ;
+    $email = $_POST['email'];
     $email = filter_var(trim($email), FILTER_SANITIZE_EMAIL);
     $password = $_POST['password'];
 
@@ -20,9 +20,20 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
     $usuario = mysqli_fetch_assoc($resultado);
 
     if ($usuario && password_verify($password, $usuario['contrasena'])) {
+        
+        if ($usuario['activo'] != "activo") {
+            echo json_encode(["status" => "inactivo", "message" => "Tu cuenta está inactiva"]);
+            $mysql->desconectar();
+            exit();
+        }
+
+
         $_SESSION['id_usuario'] = $usuario['id'];
         $_SESSION['tipo_usuario'] = $usuario['tipo'];
+        $_SESSION['activo'] = $usuario['activo'];
+
         echo json_encode(["status" => "success"]);
+        
     } else {
         echo json_encode(["status" => "error", "message" => "email o contraseña incorrectos"]);
     }
